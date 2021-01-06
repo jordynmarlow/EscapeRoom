fffbb47079d633040d14922a26ffcc82e42b9f88
Jordyn Marlow <jordynmarlow@gmail.com>
2021-01-05 22:10:15 -0500

    1/5 Commit

--- Player.gd
+++ Player.gd
@@ -1,22 +1,28 @@
 extends KinematicBody2D
 
 var MAX_SPEED = 250
+var state = 'idle'
+var dir = 'down'
 onready var motion
-onready var idle_anim = get_node("Sprite").get_node("idle_anim")
 
-func _physics_process(delta):
+func _process(delta):
 	motion = Vector2.ZERO
 	if int(Input.is_action_pressed("ui_left")) > 0:
-		idle_anim.set_frame(2)
 		motion.x -= 1
+		dir = 'left'
 	elif int(Input.is_action_pressed("ui_right")) > 0:
-		idle_anim.set_frame(0)
 		motion.x += 1
-	elif int(Input.is_action_pressed("ui_up")) > 0:
-		idle_anim.set_frame(1)
+		dir = 'right'
+	if int(Input.is_action_pressed("ui_up")) > 0:
 		motion.y -= 1
+		dir = 'up'
 	elif int(Input.is_action_pressed("ui_down")) > 0:
-		idle_anim.set_frame(3)
 		motion.y += 1
+		dir = 'down'
+	if state == 'idle' and motion != Vector2.ZERO:
+		state = 'run'
+	elif state == 'run' and motion == Vector2.ZERO:
+		state = 'idle'
+	$Sprite.animation = state + '_' + dir
 	motion = motion.normalized() * MAX_SPEED
 	motion = move_and_slide(motion)
